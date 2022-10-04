047d3af0d1ce9f6ef0dfe6b0a71539543a862699
김건희 <qorghktnqhr@gmail.com>
2022-10-04 14:44:38 +0900

    a

--- src/main/java/com/example/inputer001/problem/ProblemController.java
+++ src/main/java/com/example/inputer001/problem/ProblemController.java
@@ -1,12 +1,26 @@
 package com.example.inputer001.problem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
+@Slf4j
 public class ProblemController {
     @RequestMapping("/problem")
     public String problem() {
         return "problem";
+    }
+
+    @PostMapping("/problem_result")
+    public String problem_result(HttpServletRequest req) {
+        String test2 = req.getParameter("data");
+
+        log.info(test2);
+
+        return "redirect:problem";
     }
 }

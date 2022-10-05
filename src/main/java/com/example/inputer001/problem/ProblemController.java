d91a7a119ad784eb092769a1fc0da2a42c98254f
김건희 <qorghktnqhr@gmail.com>
2022-10-05 11:47:07 +0900

    a

--- src/main/java/com/example/inputer001/problem/ProblemController.java
+++ src/main/java/com/example/inputer001/problem/ProblemController.java
@@ -2,8 +2,10 @@ package com.example.inputer001.problem;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,5 +24,26 @@ public class ProblemController {
         log.info(test2);
 
         return "redirect:problem";
+    }
+
+    @GetMapping("/problem/ajax")
+    @ResponseBody
+    public String problem_ajax() throws InterruptedException {
+        double count = 0;
+        count = Math.random()* 1000;
+
+        Thread.sleep((int) count);
+        log.info(Thread.currentThread().toString());
+
+        return "return value";
+    }
+
+    @GetMapping("/problem/ajax2")
+    @ResponseBody
+    public String problem_ajax2(){
+        for(int i = 0; i < 10000; i++){
+            System.out.println(i);
+        }
+        return "return value2";
     }
 }
